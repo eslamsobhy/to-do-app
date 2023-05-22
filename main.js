93d0dd61ba13e01294c7ed6538646becdc69a528
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-23 00:39:24 +0300

    implemented a logging functionality using morgan package

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use((req, res, next) => {
+// logging info about all of the requests
+const morgan = require("morgan");
+const fs = require("fs");
+
+// Create a write stream (in append mode) to the log file
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "logs.log"), {
+  flags: "a",
+});
+
+// Use the "morgan" middleware and specify the log format
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use("home", (req, res, next) => {
   res.send("<h1>Hello world!</h1>");
 });
 
