c5b50c92f17e0fb4c927e7adeaa20fc96e388ffe
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-23 02:05:32 +0300

    prepared the CRUD routes for users

--- main.js
+++ main.js
@@ -6,6 +6,31 @@ const app = express();
 const logger = require("./src/controllers/loggingController");
 app.use(logger);
 
+// getting all the users
+app.get("/users", (req, res, next) => {
+  res.send({ message: "getting all users" });
+});
+
+// get user by id
+app.get("/users/:id", (req, res, next) => {
+  res.send({ message: `getting user by id: ${req.params.id}` });
+});
+
+// create a new user
+app.post("/users", (req, res, next) => {
+  res.send({ message: "user created successfully!" });
+});
+
+// update user
+app.patch("/users/:id", (req, res, next) => {
+  res.send({ message: `user ${req.params.id} has been updated successfully!` });
+});
+
+// delete user
+app.delete("/users/:id", (req, res, next) => {
+  res.send({ message: `user ${req.params.id} has been deleted successfully!` });
+});
+
 app.use("/home", (req, res, next) => {
   res.send("<h1>Hello world!</h1>");
 });
