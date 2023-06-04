216460424f9ee2503c6c4a36137e1e7fe121698b
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-06-04 21:53:24 +0300

    added the to-do-controller methods and linked them to their corresponding routes

---
+++ src/controllers/todoController.js
@@ -0,0 +1,30 @@
+const Todo = require("../models/Todo");
+const AppError = require("../utils/AppError");
+
+const getAllTodos = () => {
+  // logic goes here
+};
+
+const getTodoById = () => {
+  // logic goes here
+};
+
+const createTodo = () => {
+  // logic goes here
+};
+
+const updateTodo = () => {
+  // logic goes here
+};
+
+const deleteTodo = () => {
+  // logic goes here
+};
+
+module.exports = {
+  getAllTodos,
+  getTodoById,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,20 +3,27 @@ const express = require("express");
 const router = express.Router();
 
 // importing the CRUD logic
+const {
+  getAllTodos,
+  getTodoById,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todoController");
 
 // getting all to-do's
-router.get("/", () => {});
+router.get("/", getAllTodos);
 
 // get to-do by id
-router.get("/:id", () => {});
+router.get("/:id", getTodoById);
 
 // create a new to-do
-router.post("/", () => {});
+router.post("/", createTodo);
 
 // update to-do
-router.patch("/", () => {});
+router.patch("/:id", updateTodo);
 
 // delete to-do
-router.delete("/", () => {});
+router.delete("/:id", deleteTodo);
 
 module.exports = router;
